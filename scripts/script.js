ff82f14244717e62dcb812c227ee5468a7600bd4
Alexander Taimasov <alexander.taimasov@gmail.com>
2024-06-27 00:02:21 +0300

    worked with slick slider, added some metatags

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,41 @@
 ;(function ($, undefined) {
     $('.slider').slick( {
-        arrows: false,
-        slidesToShow: 3,
         infinite: true,
+        slidesToShow: 3,
         slidesToScroll: 3,
+        autoplay: false,
+       autoplaySpeed: 5000,
+       pauseOnFocus: true,
+       pauseOnDotsHover: true,
+       pauseOnHover: true,
+       arrows: true,
+       easing: "ease-in",
+       dots: true, 
     })
 })(jQuery);
+
+let filterItems = $('.goods-filter-item');
+let filtered = false;
+
+
+let makeFiltered = (event) => {
+    filterItems.removeClass("goods-filter-item-active");
+    $(event.target).addClass("goods-filter-item-active");
+
+ 
+    if (filtered === false) {
+        $('.slider').slick('slickFilter','.dry');
+        filtered = true;
+    } else {
+        $('.slider').slick('slickUnfilter');
+        filtered = false;
+    }
+};
+
+filterItems.on('click', makeFiltered);
+
+
+
+
+
+
