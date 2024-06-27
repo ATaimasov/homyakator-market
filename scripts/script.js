d6a10a6a5bc901e1927b92ba51999a75eee93020
Alexander Taimasov <alexander.taimasov@gmail.com>
2024-06-27 22:42:00 +0300

    added filter feature for slider, made some additions

--- scripts/script.js
+++ scripts/script.js
@@ -11,31 +11,142 @@
        arrows: true,
        easing: "ease-in",
        dots: true, 
+       variableWidth: true,
     })
 })(jQuery);
 
-let filterItems = $('.goods-filter-item');
 let filtered = false;
+let filterItems = $('.goods-filter-item');
 
-
-let makeFiltered = (event) => {
-    filterItems.removeClass("goods-filter-item-active");
-    $(event.target).addClass("goods-filter-item-active");
-
- 
+let makeFilteredDry = (event) => {
     if (filtered === false) {
-        $('.slider').slick('slickFilter','.dry');
+
+        $('.slider').slick('slickFilter', '.dry');
+        filtered = true;
+        } else {
+            $('.slider').slick('slickUnfilter');
+            filtered = false;
+        }
+
+        const target = $(event.target); 
+
+        if (target.hasClass("goods-filter-item-active")) {
+            target.removeClass("goods-filter-item-active");
+            return; 
+        }
+
+        filterItems.not(target).removeClass("goods-filter-item-active");
+        target.addClass("goods-filter-item-active");
+}
+
+$('#dry').on('click', makeFilteredDry);
+
+
+
+
+let makeFilteredExotic = (event) => {
+
+    if (filtered === false) {
+                $('.slider').slick('slickFilter', '.exotic');
+                filtered = true;
+            } else {
+                $('.slider').slick('slickUnfilter');
+                filtered = false;
+            }
+
+           
+        const target = $(event.target); 
+
+        if (target.hasClass("goods-filter-item-active")) {
+            target.removeClass("goods-filter-item-active");
+            return; 
+        }
+
+        filterItems.not(target).removeClass("goods-filter-item-active");
+        target.addClass("goods-filter-item-active"); 
+}
+
+$('#exotic').on('click', makeFilteredExotic);
+
+
+let makeFilteredMix = (event) => {
+
+    if (filtered === false) {
+        $('.slider').slick('slickFilter', '.mix');
         filtered = true;
     } else {
         $('.slider').slick('slickUnfilter');
         filtered = false;
     }
-};
+}
 
-filterItems.on('click', makeFiltered);
+$('#mix').on('click', makeFilteredMix);
 
 
+let makeFilteredSet = (event) => {
 
+    if (filtered === false) {
+        $('.slider').slick('slickFilter', '.set');
+        filtered = true;
+    } else {
+        $('.slider').slick('slickUnfilter');
+        filtered = false;
+    }
+}
+
+$('#set').on('click', makeFilteredSet);
+
+let makeFilteredNew = (event) => {
+
+    if (filtered === false) {
+        $('.slider').slick('slickFilter', '.new');
+        filtered = true;
+    } else {
+        $('.slider').slick('slickUnfilter');
+        filtered = false;
+    }
+}
+
+$('#new').on('click', makeFilteredNew);
+
+let makeFilteredGrains = (event) => {
+
+    if (filtered === false) {
+        $('.slider').slick('slickFilter', '.grains');
+        filtered = true;
+    } else {
+        $('.slider').slick('slickUnfilter');
+        filtered = false;
+    }
+}
+
+$('#grains').on('click', makeFilteredGrains);
+
+let makeFilteredDiscount = (event) => {
+
+    if (filtered === false) {
+        $('.slider').slick('slickFilter', '.discount');
+        filtered = true;
+    } else {
+        $('.slider').slick('slickUnfilter');
+        filtered = false;
+    }
+}
+
+$('#discount').on('click', makeFilteredDiscount);
+
+let makeFilteredHealthy = (event) => {
+
+    if (filtered === false) {
+        $('.slider').slick('slickFilter', '.healthy');
+        filtered = true;
+    } else {
+        $('.slider').slick('slickUnfilter');
+        filtered = false;
+    }
+}
+
+$('#healthy').on('click', makeFilteredHealthy);
 
 
 
