f57489a676e31d766fb54c723c21ea8d82b24e05
Alexander Taimasov <alexander.taimasov@gmail.com>
2024-07-01 03:18:42 +0300

    worked with slick slide

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,5 @@
 ;(function ($, undefined) {
     $('.slider').slick( {
-        infinite: true,
         slidesToShow: 3,
         slidesToScroll: 3,
         autoplay: false,
@@ -8,13 +7,32 @@
        pauseOnFocus: true,
        pauseOnDotsHover: true,
        pauseOnHover: true,
-       arrows: true,
+      arrows: false,
        easing: "ease-in",
        dots: true, 
-       variableWidth: true,
        infinite: false,
+       responsive:[
+        {
+          breakpoint: 1250,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+          {  breakpoint: 820,
+          settings: {
+            slidesToShow: 1,
+            // slidesToScroll: 1
+            // лупит бесконечно
+         } 
+        }
+        
+       ]
     })
-})(jQuery);
+
+
+
+   
 
 let filtered = false;
 let filterItems = $('.goods-filter-item');
@@ -24,9 +42,8 @@ const makeFiltered = (event, filterClass) => {
   
     if (target.hasClass("goods-filter-item-active")) {
       target.removeClass("goods-filter-item-active");
-      if (!filterItems.hasClass("goods-filter-item-active")) {
+      if (!target.hasClass("goods-filter-item-active")) {
         $('.slider').slick('slickUnfilter');
-        filtered = false;
       }
       return;
     }
@@ -51,210 +68,7 @@ $('#discount').on('click', (event) => makeFiltered(event, 'discount'));
 $('#exotic').on('click', (event) => makeFiltered(event, 'exotic'));
 $('#healthy').on('click', (event) => makeFiltered(event, 'healthy'));
 
-
-// let makeFilteredDry = (event) => {
-//     if (filtered === false) {
-
-//         $('.slider').slick('slickFilter', '.dry');
-//         filtered = true;
-//         } else {
-//             $('.slider').slick('slickUnfilter');
-//             filtered = false;
-//         }
-
-//         const target = $(event.target); 
-
-//         if (target.hasClass("goods-filter-item-active")) {
-//             target.removeClass("goods-filter-item-active");
-//             return; 
-//         }
-
-//         filterItems.not(target).removeClass("goods-filter-item-active");
-//         target.addClass("goods-filter-item-active");
-// }
-
-// $('#dry').on('click', makeFilteredDry);
-
-
-
-
-// let makeFilteredExotic = (event) => {
-
-//     if (filtered === false) {
-//                 $('.slider').slick('slickFilter', '.exotic');
-//                 filtered = true;
-//             } else {
-//                 $('.slider').slick('slickUnfilter');
-//                 filtered = false;
-//             }
-
-           
-//         const target = $(event.target); 
-
-//         if (target.hasClass("goods-filter-item-active")) {
-//             target.removeClass("goods-filter-item-active");
-//             return; 
-//         }
-
-//         filterItems.not(target).removeClass("goods-filter-item-active");
-//         target.addClass("goods-filter-item-active"); 
-// }
-
-// $('#exotic').on('click', makeFilteredExotic);
-
-
-// let makeFilteredMix = (event) => {
-
-//     if (filtered === false) {
-//         $('.slider').slick('slickFilter', '.mix');
-//         filtered = true;
-//     } else {
-//         $('.slider').slick('slickUnfilter');
-//         filtered = false;
-//     }
-
-   
-// const target = $(event.target); 
-
-// if (target.hasClass("goods-filter-item-active")) {
-//     target.removeClass("goods-filter-item-active");
-//     return; 
-// }
-
-// filterItems.not(target).removeClass("goods-filter-item-active");
-// target.addClass("goods-filter-item-active"); 
-
-// }
-
-// $('#mix').on('click', makeFilteredMix);
-
-
-// let makeFilteredSet = (event) => {
-
-//     if (filtered === false) {
-//         $('.slider').slick('slickFilter', '.set');
-//         filtered = true;
-//     } else {
-//         $('.slider').slick('slickUnfilter');
-//         filtered = false;
-//     }
-
-   
-// const target = $(event.target); 
-
-// if (target.hasClass("goods-filter-item-active")) {
-//     target.removeClass("goods-filter-item-active");
-//     return; 
-// }
-
-// filterItems.not(target).removeClass("goods-filter-item-active");
-// target.addClass("goods-filter-item-active"); 
-
-// }
-
-// $('#set').on('click', makeFilteredSet);
-
-// let makeFilteredNew = (event) => {
-
-//     if (filtered === false) {
-//         $('.slider').slick('slickFilter', '.new');
-//         filtered = true;
-//     } else {
-//         $('.slider').slick('slickUnfilter');
-//         filtered = false;
-//     }
-
-   
-// const target = $(event.target); 
-
-// if (target.hasClass("goods-filter-item-active")) {
-//     target.removeClass("goods-filter-item-active");
-//     return; 
-// }
-
-// filterItems.not(target).removeClass("goods-filter-item-active");
-// target.addClass("goods-filter-item-active"); 
-
-// }
-
-// $('#new').on('click', makeFilteredNew);
-
-// let makeFilteredGrains = (event) => {
-
-//     if (filtered === false) {
-//         $('.slider').slick('slickFilter', '.grains');
-//         filtered = true;
-//     } else {
-//         $('.slider').slick('slickUnfilter');
-//         filtered = false;
-//     }
-
-   
-// const target = $(event.target); 
-
-// if (target.hasClass("goods-filter-item-active")) {
-//     target.removeClass("goods-filter-item-active");
-//     return; 
-// }
-
-// filterItems.not(target).removeClass("goods-filter-item-active");
-// target.addClass("goods-filter-item-active"); 
-
-// }
-
-// $('#grains').on('click', makeFilteredGrains);
-
-
-// let makeFilteredDiscount = (event) => {
-
-//     if (filtered === false) {
-//         $('.slider').slick('slickFilter', '.discount');
-//         filtered = true;
-//     } else {
-//         $('.slider').slick('slickUnfilter');
-//         filtered = false;
-//     }
-
-   
-// const target = $(event.target); 
-
-// if (target.hasClass("goods-filter-item-active")) {
-//     target.removeClass("goods-filter-item-active");
-//     return; 
-// }
-
-// filterItems.not(target).removeClass("goods-filter-item-active");
-// target.addClass("goods-filter-item-active"); 
-
-// }
-
-// $('#discount').on('click', makeFilteredDiscount);
-
-
-// let makeFilteredHealthy = (event) => {
-
-//     if (filtered === false) {
-//         $('.slider').slick('slickFilter', '.healthy');
-//         filtered = true;
-//     } else {
-//         $('.slider').slick('slickUnfilter');
-//         filtered = false;
-//     }
-
-   
-// const target = $(event.target); 
-
-// if (target.hasClass("goods-filter-item-active")) {
-//     target.removeClass("goods-filter-item-active");
-//     return; 
-// }
-
-// filterItems.not(target).removeClass("goods-filter-item-active");
-// target.addClass("goods-filter-item-active"); 
-
-// }
-
-// $('#healthy').on('click', makeFilteredHealthy);
+})(jQuery);
 
 
 
